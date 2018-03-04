10becf5462f28a986e1e71693f45cb0093821320
hellokamlesh <kamlesh@iitk.ac.in>
2018-03-04 15:15:22 +0530

    push errorneous code

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-void microkernel_sendmsg(char *);
+
 void main(){
   printf("HElloword!\n");
   printf("This must be a monolithic design\n");
   microkernel_sendmsg("is more portable");
 }
-void microkernel_sendmsg(char *a) {
-  printf("microkernel:%s\n",a);
+void microkernel_getmsg(cahr *a) {
+printf("microkernel:%s\n",a);
 }
