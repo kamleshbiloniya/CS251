5746c282929265bdf8634d7def681f80024d5f40
hellokamlesh <kamlesh@iitk.ac.in>
2018-03-04 13:44:53 +0530

    local experiment

--- hello.c
+++ hello.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
-void microkernel_sendmsg(char *);
+
 void main(){
   printf("HElloword!\n");
   printf("This must be a monolithic design\n");
-  microkernel_sendmsg("is more portable");
-}
-void microkernel_sendmsg(char *a) {
-  printf("microkernel:%s\n",a);
 }
